084ec86b1651eb05d9c02ed9eb16ab7d16332e13
Jerom Ibiza <jeromibiza@gmail.com>
2025-06-17 11:33:19 +0200

    añadir controlador para stream del asistente

--- routes/assistant.routes.js
+++ routes/assistant.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const assistant = require('../controllers/assistantResponses.controller');
+const { chatStream } = require('../controllers/assistantResponses.stream');
 
 router.post('/chat', assistant.chat);
+router.post('/chat/stream', chatStream);
 
 module.exports = router;
--- routes/assistantResponses.routes.js
+++ routes/assistantResponses.routes.js
@@ -1,6 +1,6 @@
-const express     = require('express');
-const router      = express.Router();
-const assistantR  = require('../controllers/assistantResponses.controller');
+const express = require('express');
+const router = express.Router();
+const assistantR = require('../controllers/assistantResponses.controller');
 
 router.post('/assistantR/chat', assistantR.chat);
 
