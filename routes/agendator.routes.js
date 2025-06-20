ec7fec209a7413be4e7c528439d9e3c549113af1
Jerom Ibiza <jeromibiza@gmail.com>
2025-06-20 20:09:25 +0200

    ajustes en agendator

--- routes/agendator.routes.js
+++ routes/agendator.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const agendaStream = require('../controllers/agendatorResponses.stream');
+const voiceStream = require('../controllers/agendatorVoice.stream');
 
 router.post('/chat-stream', agendaStream.chatStream);
+router.post('/voice-stream', voiceStream.handleVoiceStream);
 
 module.exports = router;
